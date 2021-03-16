fb53b538d048884b120eb98a984b2f221e47836b
top.woman.in.the.mai@gmail.com <top.woman.in.the.mai@gmail.com>
2021-03-16 14:50:38 +0300

    пока воот так

---
+++ AppContext.cs
@@ -0,0 +1,28 @@
+﻿using DeliveryCore.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeliveryCore
+{
+    class AppContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Courier> Couriers { get; set; }
+        public DbSet<CourierDriver> CourierDrivers { get; set; }
+        public DbSet<Deliveryman> Deliverymans { get; set; }
+        public DbSet<Dimensions> Dimensionss { get; set; }
+        public DbSet<Machine> Machines { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=TestDB;Trusted_Connection=True");
+        }
+    }
+}
